76ae40d3953472e3285e3db1f59f1250eec3f6da
Oran Shuster <oranshuster@gmail.com>
2022-05-11 16:07:39 +0300

    fixes

--- src/test/java/com/oranshuster/webcrawlerverifier/bots/GoogleWebLightBotDataTest.java
+++ src/test/java/com/oranshuster/webcrawlerverifier/bots/GoogleWebLightBotDataTest.java
@@ -21,29 +21,29 @@ public class GoogleWebLightBotDataTest {
 
     @Test
     public void testGetIdentifier() throws Exception {
-        assertEquals(GoogleAdsBotData.getInstance().getIdentifier(), "GOOGLEWEBLIGHTBOT");
+        assertEquals(GoogleWebLightBotData.getInstance().getIdentifier(), "GOOGLEWEBLIGHTBOT");
     }
 
     @Test
     public void testGetUserAgentChecker() throws Exception {
         UserAgent ua = uaa.parse("googleweblight");
-        assertTrue(GoogleAdsBotData.getInstance().getUserAgentChecker().test(ua));
+        assertTrue(GoogleWebLightBotData.getInstance().getUserAgentChecker().test(ua));
 
         ua = uaa.parse("Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko; googleweblight) Chrome/38.0.1025.166 Mobile Safari/535.19");
-        assertTrue(GoogleAdsBotData.getInstance().getUserAgentChecker().test(ua));
+        assertTrue(GoogleWebLightBotData.getInstance().getUserAgentChecker().test(ua));
 
         ua = uaa.parse("foo Google bar");
-        assertFalse(GoogleAdsBotData.getInstance().getUserAgentChecker().test(ua));
+        assertFalse(GoogleWebLightBotData.getInstance().getUserAgentChecker().test(ua));
     }
 
     @Test
     public void testGetIps() throws Exception {
-        assertTrue(GoogleAdsBotData.getInstance().getIps().isEmpty());
+        assertTrue(GoogleWebLightBotData.getInstance().getIps().isEmpty());
     }
 
     @Test
     public void testGetHostnames() throws Exception {
-        assertEquals(GoogleAdsBotData.getInstance().getHostnames(), ImmutableList.of("googlebot.com", "google.com"));
+        assertEquals(GoogleWebLightBotData.getInstance().getHostnames(), ImmutableList.of("googlebot.com", "google.com"));
     }
 
 }
